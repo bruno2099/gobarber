90d74e206c608e632e7b5e1ae2ea0f9038104748
Bruno de Souza <bruno@vetorweb.com.br>
2020-06-29 09:22:43 -0300

    Backend em desenvolvimento

---
+++ backend/src/database/index.ts
@@ -0,0 +1,4 @@
+import { createConnection } from 'typeorm';
+
+// createConnection() - procura um arquivo chamado ormconfig.json, lê os dados deste arquivo e faz a conexão com DB
+createConnection();
---
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
+
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+// validação do token JWT
+export default function ensureAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+): void {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError('JWT token is missing', 401);
+    }
+
+    // Definimos autenticação como Bearer token
+    // 'Bearer h47411654.......'
+    const [type, token] = authHeader.split(' ');
+
+    try {
+        const decoded = verify(token, authConfig.jwt.secret);
+
+        const { sub } = decoded as TokenPayload;
+
+        request.user = {
+            id: sub,
+        };
+
+        return next();
+    } catch (error) {
+        throw new AppError('Invalid JWT token', 401);
+    }
+}
